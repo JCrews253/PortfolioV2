dfcc3dc795a566cea055321bfbc943f044aeb1c4
James Crews <jcrews206@gmail.com>
2021-11-01 17:17:13 -0700

    chess decription

--- src/branches/projects/ChessBranch.tsx
+++ src/branches/projects/ChessBranch.tsx
@@ -13,7 +13,17 @@ const ChessBranch = () => {
         branch="chess"
         iconSize={100}
       />
-      <Commit Icon={Info} title="About" content="" />
+      <Commit
+        Icon={Info}
+        title="About"
+        content="Chess AI that uses the Min Max algorithm to decided which move is best. To determine which move is best, the AI looks at 
+      every possible move for 3 turns out and assigns each move a score. The score is calculated using the current board position for each piece
+      and which pieces remain on the board. These scores are then combined to give a total score for that move. Once all scores have been calculated,
+      and placed into a decision tree, the AI chooses the score that increases its score the most during its simulated turn, and chooses the lower 
+      score when it is the opposing players turn. This alternation continues until it has reached the head of the decision tree, where then the 
+      AI will make its actual move. Some of the highlights of this project are possible move highlighting when it is the players turn, and a list of
+      all moves that have been performed during the game."
+      />
       <Commit Icon={Code} title="Tech stack" subContent="React, Typescript" />
       <Commit
         Icon={OpenInNew}
